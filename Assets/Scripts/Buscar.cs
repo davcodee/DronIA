cd51b2e63c341fa736e3c0569414e1c3f2c8ff2d
David Morales <david_ivan2402@ciencias.unam.mx>
2020-02-29 23:24:31 -0600

    Se agregó el script de buscar

---
+++ Assets/Scripts/Buscar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buscar : MonoBehaviour
+{
+     
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        if (transform.position != GameObject.Find("basee").transform.position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, GameObject.Find("basee").transform.position, 5 * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+    }
+}
